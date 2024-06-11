0e9af338e6dd31f56e20daf8d0c659b5d147e9ac
itapires <itapritsch@gmail.com>
2024-06-11 10:36:58 +0100

    6.Assign a point value to each target

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
-    private GameManager gameManager;
+    public int pointValue;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -34,7 +36,7 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        gameManager.UpdateScore(pointValue);
     }
     private void OnTriggerEnter(Collider other)
     {
