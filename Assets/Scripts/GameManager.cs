6cb071592b863495263b82c49326161bb4efc6a2
itapires <itapritsch@gmail.com>
2024-06-11 11:11:00 +0100

    3.Create GameOver function

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,7 +17,7 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
-        gameOverText.gameObject.SetActive(true);
+        
     }
 
     // Update is called once per frame
@@ -40,5 +40,9 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 }
